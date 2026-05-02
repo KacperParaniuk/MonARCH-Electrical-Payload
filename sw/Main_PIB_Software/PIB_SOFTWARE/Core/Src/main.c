dd052640fbae3c549d2e9bada08c64d796ef00af
KacperParaniuk <kacparaniuk@gmail.com>
2026-05-02 11:14:02 -0500

    SWD Setup Test #1

--- sw/Main_PIB_Software/PIB_SOFTWARE/Core/Src/main.c
+++ sw/Main_PIB_Software/PIB_SOFTWARE/Core/Src/main.c
@@ -817,9 +817,9 @@ int main(void)
 //
 //
 //
-//    /* USER CODE END WHILE */
-//
-//    /* USER CODE BEGIN 3 */
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
 
   /* USER CODE END 3 */
 }
