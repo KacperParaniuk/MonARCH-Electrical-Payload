fab539f85f5a8eec2093d49b6f2ad96a45732c2f
KacperParaniuk <kacparaniuk@gmail.com>
2026-08-24 00:24:58 -0500

    Changed driver to fix overwriting issue not DONE because issues with some functions

--- sw/Hardware-Test-Software/PIB_SOFTWARE/BSP/Sensors/Inc/ad7124_console_app.h
+++ sw/Hardware-Test-Software/PIB_SOFTWARE/BSP/Sensors/Inc/ad7124_console_app.h
@@ -68,7 +68,7 @@ typedef enum AD7124_CHIP {
 /* Public Declarations */
 int32_t ad7124_app_initialize(uint8_t configID, AD7124_CHIP chip);
 int32_t ad7124_read_device_id(AD7124_CHIP chip);
-int32_t ad7124_reset_function();
+int32_t ad7124_reset_function(AD7124_CHIP chip);
 void read_status_register(AD7124_CHIP chip);
 void read_error_register(AD7124_CHIP chip);
 
@@ -79,7 +79,7 @@ void read_error_register(AD7124_CHIP chip);
 
 int32_t menu_single_conversion(AD7124_CHIP chip);
 int32_t do_continous_conversion(uint8_t display_mode);
-void display_channel_sample(uint8_t channel, AD7124_CHIP chip);
+float display_channel_sample(uint8_t channel, AD7124_CHIP chip);
 
 
 //extern console_menu ad7124_main_menu; << not using menu
